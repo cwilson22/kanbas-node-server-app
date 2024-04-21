a9ac96ee04603aeea3836a24f1cbb47ee3bce6bf
cwilson22 <csw8@buffalo.edu>
2024-04-21 19:17:32 -0400

    kanbas quizzes fix

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ import QuizRoutes from './Kanbas/quizzes/routes.js';
 import session from "express-session";
 import "dotenv/config";
 
-const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas'
-// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
